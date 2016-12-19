363547253757180f85544daad8e34175f8b4cc13
Ondrej Rafaj <pro@Super-Dictator.local>
2016-12-19 10:56:22 +0000

    Settings works

--- Sources/App/Controllers/SettingsController.swift
+++ Sources/App/Controllers/SettingsController.swift
@@ -47,9 +47,8 @@ final class SettingsController: RootController, ControllerProtocol {
             return response
         }
         
-        let output: [String: String] = try self.getSettings()
-        
-        return JSON(try output.makeNode())
+        let output: [String: Any] = try self.getSettings()
+        return JSON(try self.makeNode(data: output))
     }
     
     func create(request: Request) throws -> ResponseRepresentable {
@@ -57,9 +56,9 @@ final class SettingsController: RootController, ControllerProtocol {
             return response
         }
         
-        let requestData: [String: Polymorphic] = self.dataFromRequest(request: request)
+        let requestData: [String: Any] = self.dataFromRequest(request: request)
         try self.saveDataToS3(requestData)
-        return JSON(try self.makeNode(polymorphicData: requestData))
+        return JSON(try self.makeNode(data: requestData))
     }
     
     func update(request: Request) throws -> ResponseRepresentable {
@@ -67,15 +66,18 @@ final class SettingsController: RootController, ControllerProtocol {
             return response
         }
         
-        var output: [String: Polymorphic] = try self.getSettings()
-        let requestData: [String: Polymorphic] = self.dataFromRequest(request: request)
+        var output: [String: Any] = try self.getSettings()
+        let requestData: [String: Any] = self.dataFromRequest(request: request)
         for key: String in requestData.keys {
-            let value: Polymorphic = requestData[key]!
-            output[key] = value
+            if let value: String = requestData[key] as? String {
+                output[key] = value
+            }
+            else if let value: Int = requestData[key] as? Int {
+                output[key] = value
+            }
         }
-        
         try self.saveDataToS3(output)
-        return JSON(try self.makeNode(polymorphicData: output))
+        return JSON(try self.makeNode(data: output))
     }
     
 }
@@ -84,34 +86,42 @@ final class SettingsController: RootController, ControllerProtocol {
 
 extension SettingsController {
     
-    func makeNode(polymorphicData: [String: Polymorphic]) throws -> Node {
-        typealias T = String
-        var newData: [String: T] = [:]
-        for key: String in polymorphicData.keys {
-            if let value: String = polymorphicData[key]!.string {
-                newData[key] = value
+    func makeNode(data: [String: Any]) throws -> Node {
+        var newData: [String: Node] = [:]
+        for key: String in data.keys {
+            if let value: String = data[key] as? String {
+                newData[key] = value.makeNode()
             }
-            else if let value: Int = polymorphicData[key]!.int {
-                newData[key] = try value
+            else if let value: Int = data[key] as? Int {
+                newData[key] = try value.makeNode()
             }
         }
         return try newData.makeNode()
     }
     
-    func saveDataToS3(_ dataToSave: [String: Polymorphic]) throws {
+    func saveDataToS3(_ dataToSave: [String: Any]) throws {
         let dic: NSDictionary = NSDictionary(dictionary: dataToSave)
         let data: Data = NSKeyedArchiver.archivedData(withRootObject: dic)
         try self.s3.put(data: data, filePath: "settings/settings.plist")
     }
     
-    func dataFromRequest(request: Request) -> [String: Polymorphic] {
+    func dataFromRequest(request: Request) -> [String: Any] {
         guard let dictionary = request.json?.object else {
             return [:]
         }
-        return dictionary
+        var newData: [String: Any] = [:]
+        for key: String in dictionary.keys {
+            if let value: String = dictionary[key]?.string {
+                newData[key] = value
+            }
+            else if let value: Int = dictionary[key]?.int {
+                newData[key] = value
+            }
+        }
+        return newData
     }
     
-    func getSettings() throws -> [String: String] {
+    func getSettings() throws -> [String: Any] {
         let data: Data
         do {
             data = try self.s3.get(fileAtPath: "settings/settings.plist")
@@ -120,10 +130,14 @@ extension SettingsController {
             return [:]
         }
         let settings: NSDictionary = NSKeyedUnarchiver.unarchiveObject(with: data) as! NSDictionary
-        var output: [String: String] = [:]
+        var output: [String: Any] = [:]
         for key: String in settings.allKeys as! [String] {
-            let value: String = settings[key] as! String
-            output[key] = value
+            if let value: String = settings[key] as? String {
+                output[key] = value
+            }
+            else if let value: Int = settings[key] as? Int {
+                output[key] = value
+            }
         }
         return output
     }
