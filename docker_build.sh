c8af7cc73e491a7d6c83bd22258a0b77ff4b9fea
Ondrej Rafaj <pro@Super-Dictator.local>
2017-04-24 21:54:22 +0100

    wip docker

--- docker_build.sh
+++ docker_build.sh
@@ -1,2 +1,5 @@
 rm -rf ./.build
-docker build -t boost .
+docker build -t mangoweb/boost .
+
+#docker build -t mangoweb/boost . && docker push mangoweb/boost:latest
+#cd ./MongoDb/ && docker build -t mangoweb/boost-mongo . && docker push mangoweb/boost-mongo:latest
--- docker_run.sh
+++ docker_run.sh
@@ -1,5 +1,7 @@
 # -p 8080:8080 // Map port of the container to the port of the host
 # -d run as daemon
 
-docker rm Boost
-docker run --privileged -i -t -p 8081:8080 --name Boost boost /bin/bash
+#docker rm Boost
+#docker run --privileged -i -t -p 8081:8080 --name Boost boost /bin/bash
+
+docker-compose up
