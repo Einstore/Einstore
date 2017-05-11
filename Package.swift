5619d748360069bc5488ad02441f4f71a30cc2c5
Ondrej Rafaj <pro@Super-Dictator.local>
2017-05-11 22:09:54 +0100

    pointer update

--- Package.swift
+++ Package.swift
@@ -12,7 +12,8 @@ import PackageDescription
 let package = Package(
     name: "Boost",
     dependencies: [
-	    .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5),
+        //.Package(url: "https://github.com/vapor/vapor.git", Version(2,0,0, prereleaseIdentifiers: ["beta"])),
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1),
         //.Package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver-Vapor.git", majorVersion: 1),
         .Package(url: "https://github.com/vapor/mongo-provider.git", majorVersion: 1, minor: 1),
         .Package(url: "https://github.com/manGoweb/S3.git", majorVersion: 1, minor: 2),
