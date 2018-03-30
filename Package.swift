dd22c959c4fd4d8b58f2662dc57874c3b20ebc2a
Ondrej Rafaj <ondrej.rafaj@gmail.com>
2018-03-30 13:04:10 +0100

    removing all else

--- Package.swift
+++ Package.swift
@@ -7,11 +7,15 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc.2"),
         .package(url: "https://github.com/vapor/fluent.git", from: "3.0.0-rc.2"),
         .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0-rc.2"),
-        .package(url: "https://github.com/vapor/jwt.git", from: "3.0.0-rc.2"),
-        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc.2"),
         .package(url: "https://github.com/kareman/SwiftShell.git", from: "4.0.2"),
+        .package(url: "https://github.com/LiveUI/ErrorsCore.git", .branch("master")),
+        .package(url: "https://github.com/LiveUI/DbCore.git", .branch("master")),
+        .package(url: "https://github.com/LiveUI/ApiCore.git", .branch("master")),
+        .package(url: "https://github.com/LiveUI/BoostCore.git", .branch("master")),
+        .package(url: "https://github.com/LiveUI/MailCore.git", .branch("master")),
+        .package(url: "https://github.com/LiveUI/SettingsCore.git", .branch("master")),
         .package(url: "https://github.com/LiveUI/VaporTestTools.git", .branch("master")),
-        .package(url: "https://github.com/twof/VaporMailgunService.git", from: "0.4.0")
+        .package(url: "https://github.com/LiveUI/FluentTestTools.git", .branch("master"))
     ],
     targets: [
         .target(
@@ -22,39 +26,9 @@ let package = Package(
             ]
         ),
         .target(
-            name: "ErrorsCore",
-            dependencies: [
-                "Vapor"
-            ]
-        ),
-        .target(
-            name: "MailCore",
-            dependencies: [
-                "Vapor",
-                "Mailgun"
-            ]
-        ),
-        .target(
-            name: "MailCoreTestTools",
-            dependencies: [
-                "Vapor",
-                "VaporTestTools",
-                "MailCore"
-            ]
-        ),
-        .target(
             name: "FileCore",
             dependencies: [
                 "Vapor",
-                "ErrorsCore"
-            ]
-        ),
-        .target(
-            name: "DbCore",
-            dependencies: [
-                "Vapor",
-                "Fluent",
-                "FluentPostgreSQL",
                 "ErrorsCore"
             ]
         ),
@@ -67,113 +41,12 @@ let package = Package(
                 "DbCore"
             ]
         ),
-        .target(
-            name: "SettingsCore",
-            dependencies: [
-                "Vapor",
-                "DbCore",
-                "ApiCore",
-                "Fluent",
-                "FluentPostgreSQL",
-                "FileCore",
-                "ErrorsCore"
-            ]
-        ),
-        .target(
-            name: "BoostCore",
-            dependencies: [
-                "Vapor",
-                "Fluent",
-                "FluentPostgreSQL",
-                "ApiCore",
-                "ErrorsCore",
-                "DbCore",
-                "SwiftShell",
-                "FileCore",
-                "MailCore",
-                "SettingsCore"
-            ]
-        ),
-        .target(
-            name: "ApiCore",
-            dependencies: [
-                "Vapor",
-                "Fluent",
-                "FluentPostgreSQL",
-                "ErrorsCore",
-                "DbCore",
-                "JWT",
-                "MailCore",
-                "Leaf"
-            ]
-        ),
-        .target(
-            name: "ApiCoreTestTools",
-            dependencies: [
-                "Vapor",
-                "ApiCore",
-                "VaporTestTools",
-                "FluentTestTools",
-                "MailCoreTestTools"
-            ]
-        ),
-        .target(
-            name: "FluentTestTools",
-            dependencies: [
-                "Vapor",
-                "VaporTestTools"
-            ]
-        ),
-        .target(
-            name: "BoostTestTools",
-            dependencies: [
-                "Vapor",
-                "ApiCore",
-                "BoostCore",
-                "VaporTestTools",
-                "ApiCoreTestTools",
-                "MailCoreTestTools"
-            ]
-        ),
         .target(name: "Run", dependencies: [
             "App"
             ]
         ),
         .testTarget(name: "AppTests", dependencies: [
             "App",
-            "VaporTestTools"
-            ]
-        ),
-        .testTarget(name: "ApiCoreTests", dependencies: [
-            "ApiCore",
-            "VaporTestTools",
-            "FluentTestTools",
-            "ApiCoreTestTools"
-            ]
-        ),
-        .testTarget(name: "SettingsCoreTests", dependencies: [
-            "SettingsCore",
-            "ApiCore",
-            "VaporTestTools",
-            "FluentTestTools",
-            "ApiCoreTestTools"
-            ]
-        ),
-        .testTarget(name: "BoostCoreTests", dependencies: [
-            "BoostCore",
-            "VaporTestTools",
-            "FluentTestTools",
-            "ApiCoreTestTools",
-            "BoostTestTools"
-            ]
-        ),
-        .testTarget(name: "DbCoreTests", dependencies: [
-            "DbCore",
-            "VaporTestTools"
-            ]
-        ),
-        .testTarget(name: "ErrorsCoreTests", dependencies: [
-            "ErrorsCore",
             "VaporTestTools"
             ]
         )
