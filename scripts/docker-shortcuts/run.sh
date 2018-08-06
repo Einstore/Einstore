f36b56b4dbdf0030368d314d01efed21a918b68f
Ondrej Rafaj <ondrej.rafaj@gmail.com>
2018-08-06 16:19:53 +0100

    wip

--- scripts/docker-shortcuts/run.sh
+++ scripts/docker-shortcuts/run.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+# Kill and remove all old containers
+docker kill $(docker ps -q)
+docker rm $(docker ps -a -q)
+
+# Build
 docker build -t boost .
 
+# Run
 docker run \
     -e APICORE_DATABASE_HOST=docker.for.mac.host.internal \
     -e APICORE_DATABASE_USER=boost \
