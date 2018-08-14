52ad6b0c99bcca49649776720b07ab51c4a4dea9
Ondrej Rafaj <ondrej.rafaj@gmail.com>
2018-08-14 12:23:13 +0100

    wip docker

--- scripts/docker-shortcuts/run.sh
+++ scripts/docker-shortcuts/run.sh
@@ -1,18 +1,16 @@
 #!/usr/bin/env bash
 
-# Kill and remove all old containers
-docker kill $(docker ps -q)
-docker rm $(docker ps -a -q)
-
 # Build
+echo "🤖 Build"
 docker build -t boost .
 
 # Run
+echo "🏃‍♀️ Run"
 docker run \
     -e APICORE_DATABASE_HOST=docker.for.mac.host.internal \
     -e APICORE_DATABASE_USER=boost \
     -e APICORE_DATABASE_DATABASE=boost \
-    -e APICORE_DATABASE_LOGGING=1 \
+    -e APICORE_DATABASE_LOGGING=1 \	
     -e APICORE_SERVER_MAX_UPLOAD_FILESIZE=500 \
     -p 8080:8080 \
     boost
