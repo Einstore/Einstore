a4e229fb3e84a3612bb844000988ed91309fb09d
Ondrej Rafaj <ondrej.rafaj@gmail.com>
2018-11-19 10:34:01 +0000

    cleaning

--- Package.swift
+++ Package.swift
@@ -5,8 +5,6 @@ let package = Package(
     name: "Boost",
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-        .package(url: "https://github.com/vapor/fluent.git", from: "3.0.0"),
-        .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0-rc.4.1"),
         .package(url: "https://github.com/LiveUI/ApiCore.git", .branch("master")),
         .package(url: "https://github.com/LiveUI/BoostCore.git", .branch("master")),
         .package(url: "https://github.com/LiveUI/VaporTestTools.git", from: "0.1.5")
