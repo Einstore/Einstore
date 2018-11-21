e9879528196702b462eba497b57838f3b3585535
Ondrej Rafaj <ondrej.rafaj@gmail.com>
2018-11-21 08:21:40 +0000

    removing test tools

--- Package.swift
+++ Package.swift
@@ -5,8 +5,7 @@ let package = Package(
     name: "Boost",
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-        .package(url: "https://github.com/LiveUI/BoostCore.git", .branch("master")),
-        .package(url: "https://github.com/LiveUI/VaporTestTools.git", from: "0.1.5")
+        .package(url: "https://github.com/LiveUI/BoostCore.git", .branch("master"))
     ],
     targets: [
         .target(
@@ -18,11 +17,6 @@ let package = Package(
         ),
        .target(name: "Run", dependencies: [
             "App"
-            ]
-        ),
-        .testTarget(name: "AppTests", dependencies: [
-            "App",
-            "VaporTestTools"
             ]
         )
     ]
